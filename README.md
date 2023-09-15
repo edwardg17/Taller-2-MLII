a8659bc72254770987083fb0dce08911341dd467
edwardg17 <30637693+edwardg17@users.noreply.github.com>
2023-09-15 10:09:55 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Taller 2 Machine Learning II - Culsterización
+Solución al taller 2 del curso Machine Learning 2 
+
+Antes de ejecutar el notebook obtenido del respositorio se recomienda verificar la compatibilidad de los modulos mediante en archivo de requirement.txt
---
+++ unsupervised/kmedoids.py
@@ -0,0 +1,144 @@
+import numpy as np
+
+class kmedoids:
+    def __init__(self):
+        pass
+
+    def distance(self, X, Y):
+        """
+        Inputs
+        X: array like with dimension M x k 
+        Y: array like with dimension N x k
+        """
+        
+        #Euclidean distance
+        x_sqrd = np.sum(np.square(X), axis=1)
+        y_sqrd = np.sum(np.square(Y), axis=1)
+
+        #Distance matrix
+        #See in deep here https://www.dabblingbadger.com/blog/2020/2/27/implementing-euclidean-distance-matrix-calculations-from-scratch-in-python
+        X_x_Y = X @ Y.T
+
+        """
+        Return
+        D: Matrix with dimension of MxN, Each value correspond to the distance
+        of the element i in X and the element j in Y
+        """
+        D = np.sqrt(abs(x_sqrd[:, np.newaxis]+y_sqrd-2*X_x_Y)) #Reshape x_sqrd to obtain the MxN matrix
+
+        return D
+
+    def rand_centroids(self, X, nclusters):
+        """
+        Inputs
+        X: array like whit dimension M x k
+        nclusters: number of clusters
+        """
+        #Dimension of the array
+        rows, colms = X.shape
+        #Initial medoid vector
+        medoids =np.empty([nclusters,colms])
+        #Obtain k random parts of the data
+        shuffled_data = np.random.permutation(X)
+        partitions    = np.array_split(shuffled_data, nclusters)
+        
+        #Obtain the centroids, mean values of the shuffle data
+        for n, part in enumerate(partitions):
+            ran_index = np.random.randint(part.shape[0])
+            medoids[n] = part[ran_index]
+        
+        """
+        Return
+        medoids: medoids vector, whit dimension k x D (k: number of clusters)
+        """
+        return medoids
+
+    def assignment_vector(self,X,medoids):
+        """
+        Inputs
+        X: array like whit dimension M x D, where D is the dimension
+        medoids: initial medoids vector, whit dimension k x D (k: number of clusters)
+        """
+        #Dimension of the array
+        rows, colms = X.shape   
+        k_index     = np.empty([rows])
+        eu_distance = self.distance(X, medoids)
+        k_index     = np.argmin(eu_distance, axis=1)
+
+        """
+        Return
+        k_index: index of the minimum distance for each row and assignment for the data
+        """
+        return k_index
+
+    def update_centroids(self,X,medoids,k_index):
+        """
+        Inputs
+        X: array like whit dimension M x D, where D is the dimension
+        medoids: old centroids medoids, whit dimension k x D (k: number of clusters)
+        k_index: index of the minimum distance for each row and assignment for the data
+        """
+        #Dimension of the array
+        rows, colms = medoids.shape
+        new_medoid  = np.empty((rows,colms))
+        for i in range(rows):
+            new_medoid[i] = np.median(X[k_index == i], axis = 0)
+        """
+        Return
+        new_medoid: new medoid vector, whit dimension k x D (k: number of clusters)
+        """    
+        return new_medoid
+
+    def losses(self,X,medoid,k_index):
+        """
+        Inputs
+        X: array like whit dimension M x D, where D is the dimension
+        medoid: medoid vector, whit dimension k x D (k: number of clusters)
+        k_index: index of the minimum distance for each row and assignment for the data
+        """    
+        #Obtain euclidian distance 
+        eu_distance = self.distance(X,medoid)
+        #initial loss value
+        loss     = 0.0
+        #Data dimension
+        rows,colms = X.shape
+
+        for i in range(rows):
+            loss = loss + np.square(eu_distance[i][k_index[i]])
+
+        """
+        Return
+        loss: objective function of Kmeans
+        """ 
+        return loss
+
+    def fit(self, X, nclusters, max_iter=100, abs_tol=1e-16, rel_tol=1e-16, printable=False):
+        """
+        Inputs
+        X: array like whit dimension M x D, where D is the dimension
+        nclusters: number of clusters
+        max_iter: maximun number of iterations 
+        tol: stop criteria
+        printable: similar to verbose, print the results of each iteration
+        """
+        medoids = self.rand_centroids(X, nclusters)
+
+        for iter in range(max_iter):
+            k_index     = self.assignment_vector(X,medoids)
+            medoids     = self.update_centroids(X,medoids,k_index)
+            loss        = self.losses(X,medoids,k_index)
+            K = medoids.shape[0]
+            if iter:
+                diff = np.abs(prev_loss - loss)
+                if diff < abs_tol and diff / prev_loss < rel_tol:
+                    break
+            prev_loss = loss
+            if printable:
+                print('iter %d, loss: %.4f' % (iter, loss))
+        """
+        Return
+        loss: Final value of objective function of Kmeans
+        mean_vector: centroids of the clusters
+        k_index: assignment vector
+        """ 
+        return loss, medoids, k_index
